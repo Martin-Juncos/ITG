20b12ac03a15b7f29a8aad3b482d80bf6c05b9e0
mjuncos <prof.mcjuncos@gmail.com>
2024-01-17 08:29:00 -0300

    Update ahorcado.py

--- ITG/ahorcado.py
+++ ITG/ahorcado.py
@@ -12,15 +12,23 @@ while intentos_restantes > 0 :
         print('Ya ingresaste esa letra')
         continue
 
+      if len(letra) != 1:
+        print('Por favor, ingrese solo una letra')
+        continue
+
+      if not letra.isalpha():
+        print('Por favor, ingrese solo letras')
+        continue
+
       if letra in palabra:
             letras_acertadas.append(letra)
-            print(letras_acertadas)
+            print('Letras acertadas: ',letras_acertadas)
             if len(palabra) == len(letras_acertadas):
                   print('Ha ganado!')
                   break
       else:
             intentos_restantes = intentos_restantes - 1
-            print(intentos_restantes)
+            print('Intentos restantes: ',intentos_restantes)
             if intentos_restantes == 0:
                   print('Looseeeerr!')
                   break
